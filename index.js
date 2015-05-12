79c5e3549682f918c81bdf36a4e361e0360d0c8e
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-05-12 12:46:01 +0200

    finished product

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 var process = require('child_process'),
-    execSync = process.execSync;
+    execSync = process.execSync,
+    LINE_BREAK = /\r?\n|\r/gi;
 
-var revision = execSync('git rev-parse HEAD');
+/**
+ * Returns the scm-source.json accoring to
+ * http://stups.readthedocs.org/en/latest/user-guide/application-development.html#docker
+ *
+ * @return {Object} scm-source.json
+ */
+function getScmSource() {
+    var revision = String(execSync('git rev-parse HEAD')),
+        url = String(execSync('git config --get remote.origin.url')),
+        status = String(execSync('git status --porcelain')),
+        author = String(execSync('id -un'));
 
-console.log(revision);
+    return {
+        revision: revision.replace(LINE_BREAK, ''),
+        url: url.replace(LINE_BREAK, ''),
+        status: status.replace(LINE_BREAK, ' ').trim(),
+        author: author.replace(LINE_BREAK, '')
+    };
+}
+
+module.exports = getScmSource;
